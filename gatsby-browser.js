8695a7be55a3b9e7d6365742f842775c673ff4f5
Yanghyeondong <hdyang0686@naver.com>
2022-08-07 14:17:59 +0900

    [fix]: git 캐시 삭제

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
-//import 'prismjs/themes/prism-material-light.css';
-import 'prismjs/themes/prism-tomorrow.css';

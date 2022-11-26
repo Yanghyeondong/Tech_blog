3240255f0b4a6381ae13fdcf9554a94c6655009e
Yanghyeondong <hdyang0686@naver.com>
2022-11-26 16:32:18 +0900

    [fix]: 글로벌 폰트 변경

--- src/components/Common/GlobalStyle.tsx
+++ src/components/Common/GlobalStyle.tsx
@@ -2,13 +2,13 @@ import React, { FunctionComponent } from 'react'
 import { Global, css } from '@emotion/react'
 
 const defaultStyle = css`
-@import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap');
 
   * {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
-    font-family: 'Nanum Gothic', sans-serif;
+    font-family: 'Noto Sans KR', sans-serif;
   }
 
   html,

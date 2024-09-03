1a623552935138c8fd9156f9b14fc852b93b79ee
Yanghyeondong <hdyang0686@naver.com>
2024-09-03 22:28:00 +0900

    🐛 리다이렉션 수정

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,1 @@
 import 'prism-themes/themes/prism-material-dark.css';
-
-export const onInitialClientRender = () => {
-    if (typeof window !== "undefined") {
-      const currentDomain = window.location.hostname;
-      const referrer = document.referrer;
-  
-      // Redirect condition
-      if (currentDomain === "yangdongs.web.app" && !referrer.includes("hyeondong.com")) {
-        window.location.replace("https://hyeondong.com");
-      }
-    }
-  };
